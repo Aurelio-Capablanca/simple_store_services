8e803be2b8735d0c3ee4da27dc445dbce7a20f82
Aurelio <aurelio.iempereur@gmail.com>
2025-11-10 00:47:38 -0600

    changes for main

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,18 @@ use axum::{Router, middleware, routing::get};
 use tower_http::cors::CorsLayer;
 
 use crate::{
-    inner::{controllers::product_controller, structures::service_structure::{self, StateService}},
-    outer::{database::connection, security::jwt_middleware::{self}},
+    inner::{
+        controllers::product_controller,
+        structures::service_structure::{self, StateService},
+    },
+    outer::{
+        database::connection,
+        security::jwt_middleware::{self},
+    },
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-   
     let state_application = std::sync::Arc::new(service_structure::StateService {
         database: connection::up_connection().await.unwrap(),
     });
@@ -25,20 +30,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         axum::http::header::CONTENT_TYPE,
     ]);
 
-    let public_routes: Router<Arc<StateService>> = Router::new()
-    .route("/", get(product_controller::hello_world));
+    let public_routes: Router<Arc<StateService>> =
+        Router::new().route("/", get(product_controller::hello_world));
 
-    let private_routes : Router<Arc<StateService>> = Router::new()
-    .route("/test-access", get(product_controller::tester_secured))
-    .route("/test-identity", get(product_controller::test_identities))
-    .route_layer(middleware::from_fn(jwt_middleware::jwt_middleware));
+    let private_routes: Router<Arc<StateService>> = Router::new()
+        .route("/test-access", get(product_controller::tester_secured))
+        .route("/test-identity", get(product_controller::test_identities))
+        .route_layer(middleware::from_fn(jwt_middleware::jwt_middleware));
 
-    let application : Router = Router::new()
+    let application: Router = Router::new()
         .merge(public_routes)
         .nest("/api", private_routes)
         .with_state(state_application)
         .layer(cors);
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:9091").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:9091")
+        .await
+        .unwrap();
     axum::serve(listener, application).await.unwrap();
     Ok(())
 }
