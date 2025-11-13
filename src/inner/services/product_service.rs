5ce011c85cbca84228ce65229d1cedc9f110f44e
Aurelio <aurelio.iempereur@gmail.com>
2025-11-12 23:47:38 -0600

    adding products
    + leading to a larger codebase

--- src/inner/services/product_service.rs
+++ src/inner/services/product_service.rs
@@ -64,27 +64,87 @@ pub async fn load_products(
      unique_code, product_stock_number, is_discontinued)
     VALUES(0, '', '', 0, b'0', b'0', b'0', '', 0, '', 0, 0); */
     let product_list = loader.list_product;
-    product_list.iter().for_each(|product| {
-        
-    });
+    let mut product_ids: Vec<u64> = Vec::with_capacity(product_list.len());
 
-    let executioner_two = sqlx::query(
-        "INSERT INTO simple_store.product
-    (product_name, 
-    product_description, 
-    product_price,
-    has_discount, 
-    has_stock, 
-    is_available, 
-    expiring_date, 
-    id_category,
-    unique_code, 
-    product_stock_number, 
-    is_discontinued)
-    VALUES(?, ?, ?, ?, ?, ?, ?,?, ?,?,?);",
-    )
-    .execute(&mut *transaction)
-    .await;
+    for product in product_list {
+        let has_discount = if product.has_discount.unwrap_or(false) {
+            1u8
+        } else {
+            0u8
+        };
+        let has_stock = if product.has_stock.unwrap_or(false) {
+            1u8
+        } else {
+            0u8
+        };
+        let is_available = if product.is_available.unwrap_or(false) {
+            1u8
+        } else {
+            0u8
+        };
+
+        let date_expiration = match NaiveDate::parse_from_str(
+            product.expiring_date
+                .as_deref()
+                .unwrap_or(Utc::now().format("%Y-%m-%d").to_string().as_str()),
+            "%Y-%m-%d",
+        ) {
+            Ok(date) => date.and_hms_opt(0, 0, 0).unwrap(),
+            Err(err) => {
+                return Err(Box::new(err));
+            }
+        };
+
+        let is_discontinued = if product.is_discontinued.unwrap_or(false){
+            1u8
+        } else {
+            0u8
+        };
+
+        let executioner_two = sqlx::query(
+            "INSERT INTO simple_store.product
+            (product_name, 
+            product_description, 
+            product_price,
+            has_discount, 
+            has_stock, 
+            is_available, 
+            expiring_date, 
+            id_category,
+            unique_code, 
+            product_stock_number, 
+            is_discontinued)
+            VALUES(?, ?, ?, ?, ?, ?, ?,?, ?,?,?);",
+        )
+        .bind(&product.product_name.unwrap_or("No Name".to_string()))
+        .bind(
+            &product
+                .product_description
+                .unwrap_or("no Description".to_string())
+                .to_string(),
+        )
+        .bind(has_discount)
+        .bind(has_stock)
+        .bind(is_available)
+        .bind(date_expiration)
+        .bind(&product.id_category.unwrap_or(0))
+        .bind("RandomCodeForProducts")
+        .bind(&product.product_stock_number.unwrap_or(0))
+        .bind(is_discontinued)
+        .execute(&mut *transaction)
+        .await;
+
+        let id_product = match executioner_two {
+            Ok(row) => {
+                row.last_insert_id()
+            },
+            Err(err) => {
+                transaction.rollback().await?;
+                return Err(Box::new(err));
+            }
+        };
+        product_ids.push(id_product);
+    }
 
     Ok(String::new())
 }
