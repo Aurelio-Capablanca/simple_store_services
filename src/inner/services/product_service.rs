be82cf596e69675ee60754cd7cdacdaa0ffe275e
Aurelio <aurelio.iempereur@gmail.com>
2025-11-13 20:51:36 -0600

    expanding missing features
    + validating and parsing properly

--- src/inner/services/product_service.rs
+++ src/inner/services/product_service.rs
@@ -23,8 +23,6 @@ pub async fn load_products(
     id_store, id_retailer)
     VALUES(0, 0, '', 0, 0); */
     let retailer_data = &loader.retailer_bill;
-    let message_error: String;
-    let status: bool;
 
     let date_bill = match NaiveDate::parse_from_str(
         retailer_data
@@ -38,8 +36,25 @@ pub async fn load_products(
             return Err(Box::new(err));
         }
     };
-
     let mut transaction = state.database.begin().await?;
+
+    let seek_store = sqlx::query(
+        "
+    SELECT id_store
+    FROM simple_store.store
+    Where id_store = ?;
+    ",
+    )
+    .bind(&retailer_data.id_store)
+    .execute(&mut *transaction)
+    .await;
+
+    match seek_store {
+        Ok(_) => {},
+        Err(err) => {
+            return Err(Box::new(err));
+        }
+    }
 
     // Insert retailer bill
     let executioner_one = sqlx::query(
@@ -68,9 +83,24 @@ pub async fn load_products(
      unique_code, product_stock_number, is_discontinued)
     VALUES(0, '', '', 0, b'0', b'0', b'0', '', 0, '', 0, 0); */
     let product_list = loader.list_product;
-    let mut product_intermediary : Vec<ProductIntermediary> = Vec::with_capacity(product_list.len());
+    let mut product_intermediary: Vec<ProductIntermediary> = Vec::with_capacity(product_list.len());
+    let mut total_bill_due: f64 = 0f64;
 
-    for product in product_list {
+    for product in product_list {        
+        let buy_price_product = product.buying_price.unwrap_or(0f64);
+        total_bill_due += &product.buying_price.unwrap_or(0f64);
+        //if the Id is present don't make inserts, ignore and left over the object
+        if let Some(id_product_or) = product.id_product {
+            let id = match u64::try_from(id_product_or) {
+                Ok(res) => res,
+                Err(err) => {
+                    return Err(Box::new(err));
+                }
+            };
+            product_intermediary.push(ProductIntermediary { id: id, buy_price: buy_price_product });
+            continue;
+        }
+
         let name = &product.product_name.unwrap_or("No Name".to_string());
         let has_discount = if product.has_discount.unwrap_or(false) {
             1u8
@@ -155,24 +185,78 @@ pub async fn load_products(
                 return Err(Box::new(err));
             }
         };
-        let buy_price_product = product.buying_price.unwrap_or(0f64);        
-        product_intermediary.push(ProductIntermediary { id: id_product, buy_price: buy_price_product });
+        
+        product_intermediary.push(ProductIntermediary {
+            id: id_product,
+            buy_price: buy_price_product,
+        });
     }
 
-    for id_product in product_intermediary {
-
+    for product in product_intermediary {
         //Product Bill
         //---------------
         /*INSERT INTO simple_store.product_bill
         (id_product, id_bill, buying_price)
         VALUES(0, 0, 0); */
+        let executioner_three = sqlx::query(
+            "INSERT INTO simple_store.product_bill
+                (id_product, id_bill, buying_price)
+                VALUES(?, ?, ?);",
+        )
+        .bind(&product.id)
+        .bind(&id_bill_retailer)
+        .bind(&product.buy_price)
+        .execute(&mut *transaction)
+        .await;
 
+        match executioner_three {
+            Ok(_) => {
+                print!("goes right!")
+            }
+            Err(err) => {
+                transaction.rollback().await?;
+                return Err(Box::new(err));
+            }
+        }
         //Product to Store
         //---------------
         /*INSERT INTO simple_store.product_location
         (id_product, id_store)
         VALUES(0, 0); */
+        let executioner_four = sqlx::query(
+            "INSERT INTO simple_store.product_location
+            (id_product, id_store)
+            VALUES(?, ?);",
+        )
+        .bind(&product.id)
+        .bind(&retailer_data.id_store)
+        .execute(&mut *transaction)
+        .await;
+
+        match executioner_four {
+            Ok(_) => {}
+            Err(err) => {
+                transaction.rollback().await?;
+                return Err(Box::new(err));
+            }
+        }
     }
+
+    //Update Total_capital at Store
+    /*UPDATE simple_store.store
+    SET store_number=0, store_location=0, total_capital=0, store_name=''
+    WHERE id_store=0; */
+    let executor_five = sqlx::query(
+        "
+    UPDATE simple_store.store
+    SET total_capital=?
+    WHERE id_store=?;
+    ",
+    )
+    .bind(&total_bill_due)
+    .bind(&retailer_data.id_store)
+    .execute(&mut *transaction)
+    .await;
 
     transaction.commit().await?;
 
